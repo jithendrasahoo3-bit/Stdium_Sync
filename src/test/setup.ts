73086e4bb4cd3d7e1333afdaedca150204ffe250
jithendrasahoo3-bit <jithendrasahoo3@gmail.com>
2026-08-19 17:25:26 +0530

    Delete src/test directory

--- src/test/setup.ts
+++
@@ -1,5 +0,0 @@
-import '@testing-library/jest-dom';
-import { vi } from 'vitest';
-
-// Mock scrollIntoView since JSDOM does not implement layout methods
-window.HTMLElement.prototype.scrollIntoView = vi.fn();
